75b28305d4490e21b2320e5d66c3015aae2d8f62
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-06-15 17:03:22 -0400

    Initial commit of Terraform and Kubernetes project

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
